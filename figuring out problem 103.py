df03eeb0694a9a84a92efad51b1b60076524a113
IsaGraceGuthrie <116840075+IsaGraceGuthrie@users.noreply.github.com>
2022-11-08 16:11:39 -0500

    Add files via upload

---
+++ figuring out problem 103.py
@@ -0,0 +1,274 @@
+Python 3.10.6 (v3.10.6:9c7b4bd164, Aug  1 2022, 17:13:48) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================ RESTART: /Users/isabelaguthrie/Desktop/103r.py ================
+freq_last_inital(in_class)
+{'s': 1}
+
+
+================ RESTART: /Users/isabelaguthrie/Desktop/103r.py ================
+freq_last_inital(in_class)
+
+{'s': 1}
+
+================ RESTART: /Users/isabelaguthrie/Desktop/103r.py ================
+freq_last_inital(in_class)
+
+{'s': 1}
+
+================ RESTART: /Users/isabelaguthrie/Desktop/103r.py ================
+freq_last_inital(in_class)
+
+{'skrip': 1}
+
+================ RESTART: /Users/isabelaguthrie/Desktop/103r.py ================
+Traceback (most recent call last):
+  File "/Users/isabelaguthrie/Desktop/103r.py", line 20, in <module>
+    freq_last_inital(in_class)
+  File "/Users/isabelaguthrie/Desktop/103r.py", line 14, in freq_last_inital
+    d[char]=1
+TypeError: unhashable type: 'list'
+
+================ RESTART: /Users/isabelaguthrie/Desktop/103r.py ================
+Traceback (most recent call last):
+  File "/Users/isabelaguthrie/Desktop/103r.py", line 20, in <module>
+    freq_last_inital(in_class)
+  File "/Users/isabelaguthrie/Desktop/103r.py", line 14, in freq_last_inital
+    d[char]=1
+TypeError: unhashable type: 'list'
+
+================ RESTART: /Users/isabelaguthrie/Desktop/103r.py ================
+Traceback (most recent call last):
+  File "/Users/isabelaguthrie/Desktop/103r.py", line 20, in <module>
+    freq_last_inital(in_class)
+  File "/Users/isabelaguthrie/Desktop/103r.py", line 14, in freq_last_inital
+    d[char]=1
+TypeError: unhashable type: 'list'
+
+================ RESTART: /Users/isabelaguthrie/Desktop/103r.py ================
+freq_last_inital(in_class)
+
+{'skrip': 1}
+
+================ RESTART: /Users/isabelaguthrie/Desktop/103r.py ================
+freq_last_inital(in_class)
+
+{'Hongell': 1}
+
+================ RESTART: /Users/isabelaguthrie/Desktop/103r.py ================
+freq_last_inital(in_class)
+
+{'skrip': 1}
+
+================ RESTART: /Users/isabelaguthrie/Desktop/103r.py ================
+freq_last_inital(in_class)
+
+{'Hongell': 1}
+
+================ RESTART: /Users/isabelaguthrie/Desktop/103r.py ================
+freq_last_inital(in_class)
+
+{'skrip': 1}
+
+================ RESTART: /Users/isabelaguthrie/Desktop/103r.py ================
+freq_last_inital(in_class)
+
+{'skrip': 1}
+
+================ RESTART: /Users/isabelaguthrie/Desktop/103r.py ================
+freq_last_inital(in_class)
+
+{'s': 1}
+
+================ RESTART: /Users/isabelaguthrie/Desktop/103r.py ================
+freq_last_inital(in_class)
+
+{'s': 1}
+
+================ RESTART: /Users/isabelaguthrie/Desktop/103r.py ================
+Traceback (most recent call last):
+  File "/Users/isabelaguthrie/Desktop/103r.py", line 21, in <module>
+    freq_last_inital(in_class)
+  File "/Users/isabelaguthrie/Desktop/103r.py", line 15, in freq_last_inital
+    d[char]+=1
+KeyError: 'holly'
+
+================ RESTART: /Users/isabelaguthrie/Desktop/103r.py ================
+freq_last_inital(in_class)
+
+{'holly': 1, 'skrip': 1, 'Danielle': 1, 'Hongell': 1, 'Christina': 1, 'Shadid': 1, 'Adrianne': 1, 'Verstraete': 1, 'Ellen': 1, 'Kevin': 1, 'Julia': 1, 'Schutz': 1, 'Cody': 1, 'Brown': 1, 'Amanda': 1, 'Miloserny': 1, 'Catherine': 1, 'Coherty': 1, 'Kaylen': 1, 'Nyhuis': 1, 'Brenna': 1, 'Kieft': 1, 'Emily': 1, 'Rusch': 1, 'Britney': 1, 'Salazar': 1, 'Margaret': 1, 'Dunn': 1, 'Camryn': 1, 'Hurley': 1, 'Isabela': 1, 'Guthrie Beckstrom': 1, 'Cadyn': 1, 'Reger': 1, 'Haley': 1, 'Greene': 1, 'Sarah': 1, 'Wardlow': 1, 'Najmeh': 1, 'Salehi': 1}
+
+================ RESTART: /Users/isabelaguthrie/Desktop/103r.py ================
+freq_last_inital(in_class)
+
+{'holly': 1, 'skrip': 1, 'Danielle': 1, 'Hongell': 1, 'Christina': 1, 'Shadid': 1, 'Adrianne': 1, 'Verstraete': 1, 'Ellen': 1, 'Kevin': 1, 'Julia': 1, 'Schutz': 1, 'Cody': 1, 'Brown': 1, 'Amanda': 1, 'Miloserny': 1, 'Catherine': 1, 'Coherty': 1, 'Kaylen': 1, 'Nyhuis': 1, 'Brenna': 1, 'Kieft': 1, 'Emily': 1, 'Rusch': 1, 'Britney': 1, 'Salazar': 1, 'Margaret': 1, 'Dunn': 1, 'Camryn': 1, 'Hurley': 1, 'Isabela': 1, 'Guthrie Beckstrom': 1, 'Cadyn': 1, 'Reger': 1, 'Haley': 1, 'Greene': 1, 'Sarah': 1, 'Wardlow': 1, 'Najmeh': 1, 'Salehi': 1}
+
+================ RESTART: /Users/isabelaguthrie/Desktop/103r.py ================
+freq_last_inital(in_class)
+
+{'holly': 1, 'skrip': 1, 'Danielle': 1, 'Hongell': 1, 'Christina': 1, 'Shadid': 1, 'Adrianne': 1, 'Verstraete': 1, 'Ellen': 1, 'Kevin': 1, 'Julia': 1, 'Schutz': 1, 'Cody': 1, 'Brown': 1, 'Amanda': 1, 'Miloserny': 1, 'Catherine': 1, 'Coherty': 1, 'Kaylen': 1, 'Nyhuis': 1, 'Brenna': 1, 'Kieft': 1, 'Emily': 1, 'Rusch': 1, 'Britney': 1, 'Salazar': 1, 'Margaret': 1, 'Dunn': 1, 'Camryn': 1, 'Hurley': 1, 'Isabela': 1, 'Guthrie Beckstrom': 1, 'Cadyn': 1, 'Reger': 1, 'Haley': 1, 'Greene': 1, 'Sarah': 1, 'Wardlow': 1, 'Najmeh': 1, 'Salehi': 1}
+
+================ RESTART: /Users/isabelaguthrie/Desktop/103r.py ================
+freq_last_inital(in_class)
+
+{}
+
+================ RESTART: /Users/isabelaguthrie/Desktop/103r.py ================
+freq_last_inital(in_class)
+'
+SyntaxError: multiple statements found while compiling a single statement
+freq_last_inital(in_class)
+
+{'holly': 1, 'skrip': 1, 'Danielle': 1, 'Hongell': 1, 'Christina': 1, 'Shadid': 1, 'Adrianne': 1, 'Verstraete': 1, 'Ellen': 1, 'Kevin': 1, 'Julia': 1, 'Schutz': 1, 'Cody': 1, 'Brown': 1, 'Amanda': 1, 'Miloserny': 1, 'Catherine': 1, 'Coherty': 1, 'Kaylen': 1, 'Nyhuis': 1, 'Brenna': 1, 'Kieft': 1, 'Emily': 1, 'Rusch': 1, 'Britney': 1, 'Salazar': 1, 'Margaret': 1, 'Dunn': 1, 'Camryn': 1, 'Hurley': 1, 'Isabela': 1, 'Guthrie Beckstrom': 1, 'Cadyn': 1, 'Reger': 1, 'Haley': 1, 'Greene': 1, 'Sarah': 1, 'Wardlow': 1, 'Najmeh': 1, 'Salehi': 1}
+
+================ RESTART: /Users/isabelaguthrie/Desktop/103r.py ================
+freq_last_inital(in_class)
+
+{'holly': 1, 'skrip': 1, 'Danielle': 1, 'Hongell': 1, 'Christina': 1, 'Shadid': 1, 'Adrianne': 1, 'Verstraete': 1, 'Ellen': 1, 'Kevin': 1, 'Julia': 1, 'Schutz': 1, 'Cody': 1, 'Brown': 1, 'Amanda': 1, 'Miloserny': 1, 'Catherine': 1, 'Coherty': 1, 'Kaylen': 1, 'Nyhuis': 1, 'Brenna': 1, 'Kieft': 1, 'Emily': 1, 'Rusch': 1, 'Britney': 1, 'Salazar': 1, 'Margaret': 1, 'Dunn': 1, 'Camryn': 1, 'Hurley': 1, 'Isabela': 1, 'Guthrie Beckstrom': 1, 'Cadyn': 1, 'Reger': 1, 'Haley': 1, 'Greene': 1, 'Sarah': 1, 'Wardlow': 1, 'Najmeh': 1, 'Salehi': 1}
+
+================ RESTART: /Users/isabelaguthrie/Desktop/103r.py ================
+Traceback (most recent call last):
+  File "/Users/isabelaguthrie/Desktop/103r.py", line 19, in <module>
+    freq_last_inital(in_class)
+  File "/Users/isabelaguthrie/Desktop/103r.py", line 5, in freq_last_inital
+    for char[1::2] in x:
+UnboundLocalError: local variable 'char' referenced before assignment
+
+= RESTART: /Users/isabelaguthrie/Desktop/Sophmore Year /Compture Science /lab /histogram.py
+histogram('parrot')
+{'p': 1, 'a': 1, 'r': 2, 'o': 1, 't': 1}
+
+================ RESTART: /Users/isabelaguthrie/Desktop/103r.py ================
+Traceback (most recent call last):
+  File "/Users/isabelaguthrie/Desktop/103r.py", line 18, in <module>
+    freq_last_inital(in_class)
+  File "/Users/isabelaguthrie/Desktop/103r.py", line 5, in freq_last_inital
+    for char[1::2] in x:
+UnboundLocalError: local variable 'char' referenced before assignment
+
+================ RESTART: /Users/isabelaguthrie/Desktop/103r.py ================
+Traceback (most recent call last):
+  File "/Users/isabelaguthrie/Desktop/103r.py", line 18, in <module>
+    freq_last_inital(in_class)
+  File "/Users/isabelaguthrie/Desktop/103r.py", line 5, in freq_last_inital
+    for char[1::2] in x:
+UnboundLocalError: local variable 'char' referenced before assignment
+
+================ RESTART: /Users/isabelaguthrie/Desktop/103r.py ================
+Traceback (most recent call last):
+  File "/Users/isabelaguthrie/Desktop/103r.py", line 19, in <module>
+    freq_last_inital(in_class)
+  File "/Users/isabelaguthrie/Desktop/103r.py", line 5, in freq_last_inital
+    char=word[1::2]
+NameError: name 'word' is not defined. Did you mean: 'ord'?
+
+================ RESTART: /Users/isabelaguthrie/Desktop/103r.py ================
+freq_last_inital(in_class)
+
+{'h': 1, 's': 1, 'D': 1, 'H': 1, 'C': 1, 'S': 1, 'A': 1, 'V': 1, 'E': 1, 'K': 1, 'J': 1, 'B': 1, 'M': 1, 'N': 1, 'R': 1, 'I': 1, 'G': 1, 'W': 1}
+
+========================== RESTART: /Users/isabelaguthrie/Desktop/103r.py ==========================
+freq_last_inital(in_class)
+
+{'h': 1, 'S': 1, 'D': 1, 'H': 1, 'C': 1, 'A': 1, 'V': 1, 'E': 1, 'K': 1, 'J': 1, 'B': 1, 'M': 1, 'N': 1, 'R': 1, 'I': 1, 'G': 1, 'W': 1}
+
+========================== RESTART: /Users/isabelaguthrie/Desktop/103r.py ==========================
+freq_last_inital(in_class)
+
+{'h': 1, 'S': 1, 'D': 1, 'H': 1, 'C': 1, 'A': 1, 'V': 1, 'E': 1, 'K': 1, 'J': 1, 'B': 1, 'M': 1, 'N': 1, 'R': 1, 'I': 1, 'G': 1, 'W': 1}
+
+===== RESTART: /Users/isabelaguthrie/Desktop/Sophmore Year /Compture Science /lab /histogram.py ====
+histogram('parrot')
+{'p': 1, 'a': 1, 'r': 2, 'o': 1, 't': 1}
+
+===== RESTART: /Users/isabelaguthrie/Desktop/Sophmore Year /Compture Science /lab /histogram.py ====
+histogram('parrot')
+{'p': 1, 'a': 1, 'r': 2, 'o': 1, 't': 1}
+hist=histogram('parrot')
+hist
+{'p': 1, 'a': 1, 'r': 2, 'o': 1, 't': 1}
+inverse=invert_dict(hist)
+inverse
+{1: ['t'], 2: ['r']}
+
+===== RESTART: /Users/isabelaguthrie/Desktop/Sophmore Year /Compture Science /lab /histogram.py ====
+freq_last_inital(in_class)
+
+Traceback (most recent call last):
+  File "<pyshell#30>", line 1, in <module>
+    freq_last_inital(in_class)
+NameError: name 'freq_last_inital' is not defined
+hist=histogram('parrot')
+
+hist
+{'p': 1, 'a': 1, 'r': 2, 'o': 1, 't': 1}
+inverse=invert_dict(hist)
+
+
+inverse
+{1: ['p', 'a', 'o', 't'], 2: ['r']}
+
+========================== RESTART: /Users/isabelaguthrie/Desktop/103r.py ==========================
+freq_last_inital(in_class)
+
+{'h': 1, 'S': 1, 'D': 1, 'H': 1, 'C': 1, 'A': 1, 'V': 1, 'E': 1, 'K': 1, 'J': 1, 'B': 1, 'M': 1, 'N': 1, 'R': 1, 'I': 1, 'G': 1, 'W': 1}
+
+===== RESTART: /Users/isabelaguthrie/Desktop/Sophmore Year /Compture Science /lab /histogram.py ====
+t=[1,2,3]
+d=dict()
+d[t]='oops'
+Traceback (most recent call last):
+  File "<pyshell#39>", line 1, in <module>
+    d[t]='oops'
+TypeError: unhashable type: 'list'
+t[0]='oops'
+t
+['oops', 2, 3]
+d[t]
+Traceback (most recent call last):
+  File "<pyshell#42>", line 1, in <module>
+    d[t]
+TypeError: unhashable type: 'list'
+
+========================== RESTART: /Users/isabelaguthrie/Desktop/103r.py ==========================
+freq_last_inital(in_class)
+
+{'S': 1, 'H': 1, 'V': 1, 'K': 1, 'B': 1, 'M': 1, 'C': 1, 'N': 1, 'R': 1, 'D': 1, 'G': 1, 'W': 1}
+
+========================== RESTART: /Users/isabelaguthrie/Desktop/103r.py ==========================
+freq_last_inital(in_class)
+
+{'S': 1, 'H': 1, 'V': 1, 'K': 1, 'B': 1, 'M': 1, 'C': 1, 'N': 1, 'R': 1, 'D': 1, 'G': 1, 'W': 1}
+
+========================== RESTART: /Users/isabelaguthrie/Desktop/103r.py ==========================
+Traceback (most recent call last):
+  File "/Users/isabelaguthrie/Desktop/103r.py", line 18, in <module>
+    freq_last_inital(in_class)
+  File "/Users/isabelaguthrie/Desktop/103r.py", line 10, in freq_last_inital
+    d[char]+=1
+KeyError: 'S'
+
+========================== RESTART: /Users/isabelaguthrie/Desktop/103r.py ==========================
+freq_last_inital(in_class)
+
+{'S': 1, 'H': 1, 'V': 1, 'K': 1, 'B': 1, 'M': 1, 'C': 1, 'N': 1, 'R': 1, 'D': 1, 'G': 1, 'W': 1}
+
+========================== RESTART: /Users/isabelaguthrie/Desktop/103r.py ==========================
+freq_last_inital(in_class)
+
+{'S': 1, 'H': 1, 'V': 1, 'K': 1, 'B': 1, 'M': 1, 'C': 1, 'N': 1, 'R': 1, 'D': 1, 'G': 1, 'W': 1}
+
+========================== RESTART: /Users/isabelaguthrie/Desktop/103r.py ==========================
+freq_last_inital(in_class)
+
+{'S': 1, 'H': 1, 'V': 1, 'K': 1, 'B': 1, 'M': 1, 'C': 1, 'N': 1, 'R': 1, 'D': 1, 'G': 1, 'W': 1}
+
+========================== RESTART: /Users/isabelaguthrie/Desktop/103r.py ==========================
+freq_last_inital(in_class)
+
+{'S': 1, 'H': 1, 'V': 1, 'K': 1, 'B': 1, 'M': 1, 'C': 1, 'N': 1, 'R': 1, 'D': 1, 'G': 1, 'W': 1}
+
+========================== RESTART: /Users/isabelaguthrie/Desktop/103r.py ==========================
+freq_last_inital(in_class)
+
+{'S': 5, 'H': 2, 'V': 1, 'K': 2, 'B': 1, 'M': 1, 'C': 1, 'N': 1, 'R': 2, 'D': 1, 'G': 2, 'W': 1}
